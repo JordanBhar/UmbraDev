c99b9d567418cf519de3a42d23613188d799a37f
JordanBhar <jordan.bhar@hotmail.com>
2022-06-14 18:27:01 -0400

    Commit 15: Next Test

--- js/main.js
+++ js/main.js
@@ -6,13 +6,14 @@ function scrollTransparentNavbar (){
     
     var navbar = document.getElementById("navTransparent")
     
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    
+    if (document.body.scrollTop > 100|| document.documentElement.scrollTop > 100){
         navbar.style.backgroundColor = 'rgb(72, 0, 155)';
         navbar.style.zIndex = "2"
         navbar.style.transition = "1s"
         
     }else{
-        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
         navbar.style.zIndex = "1"
     }
 
