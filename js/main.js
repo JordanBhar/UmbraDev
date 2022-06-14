581370c42c9d3bd9b58c0827a205c8a457c97c97
JordanBhar <jordan.bhar@hotmail.com>
2022-06-14 16:33:12 -0400

    Commit 13: Backroads Test

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
 window.onscroll = function(){scrollTransparentNavbar()} 
 
 function scrollTransparentNavbar (){
-
-    console.log(document.body.scrollTop)
+    
     var navbar = document.getElementById("navTransparent")
-    console.log(navbar)
-
+    
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
         navbar.style.backgroundColor = 'rgb(72, 0, 155)';
         navbar.style.zIndex = "2"
